9b9c440382a38e4ad0b142a484dc823090b680ce
Tiago Tenório <tiagotenoriogitcommits@gmail.com>
2023-06-01 11:12:49 -0300

    Segundo commit desta aplicação já com autorização de login e conexão com o firebase.

--- app/src/main/java/com/example/meuslivros/MainActivity.kt
+++ app/src/main/java/com/example/meuslivros/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.meuslivros
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.meuslivros.databinding.ActivityMainBinding
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var googleSignInCliente: GoogleSignInClient
 
+    private lateinit var auth: FirebaseAuth;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,5 +26,39 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
 
         setContentView(view)
+
+        auth = Firebase.auth
+
+        binding.botaoEntrar.setOnClickListener {
+
+
+                auth.signInWithEmailAndPassword(binding.editTextUsuario.text.toString(),binding.editTextSenha.text.toString())
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCustomToken:success")
+                            val user = auth.currentUser
+                            Toast.makeText(baseContext, "Autenticação efetuada",
+                                Toast.LENGTH_SHORT).show()
+                            //updateUI(user)
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCustomToken:failure", task.exception)
+                            Toast.makeText(baseContext, "Erro de autenticação",
+                                Toast.LENGTH_SHORT).show()
+                            //updateUI(null)
+                        }
+                    }
+
+
+        }
+
+
+    }
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        //val currentUser = auth.currentUser
+        //updateUI(currentUser)
     }
 }
